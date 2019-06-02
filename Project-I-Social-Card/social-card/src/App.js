0b391db657cfda106c0a9a09d87498e5fa7044e7
Raymond Thurman <raymondthurman5@gmail.com>
2019-06-02 12:41:53 -0400

    playing with styled components

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import './App.css';
 import './components/HeaderComponents/HeaderContainer';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
