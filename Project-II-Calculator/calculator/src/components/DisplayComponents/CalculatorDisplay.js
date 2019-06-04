b88ea6682919532bda06e3e7c2b540cc0d09b15b
Raymond Thurman <raymondthurman5@gmail.com>
2019-06-03 20:19:10 -0400

    transfering from master to my branch

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 import './Display.css';
+
+const CalculatorDisplay = () => {
+	return (
+		<React.Fragment>
+			<Wrapper>
+				<CalcInput type='text' placeholder='0' />
+			</Wrapper>
+		</React.Fragment>
+	);
+};
+
+const Wrapper = styled.div`
+	width: 100%;
+	height: 75px;
+`;
+
+const CalcInput = styled.input`
+	width: 100%;
+	height: 75px;
+	font-size: 4rem;
+	padding: .5rem;
+	background-color: #333;
+	color: #fff;
+	text-align: right;
+	border: 1px solid white;
+`;
+
+export default CalculatorDisplay;
