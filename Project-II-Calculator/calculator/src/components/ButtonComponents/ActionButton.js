cee1cd6dba7cb103838c2ce4f90d2bab73d91bef
Raymond Thurman <raymondthurman5@gmail.com>
2019-06-03 20:29:38 -0400

    Action button component initially made

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 import './Button.css';
+
+const ActionButton = (props) => {
+	return (
+		<React.Fragment>
+			<Wrapper>
+				<ActionBtn>{props.text}</ActionBtn>
+			</Wrapper>
+		</React.Fragment>
+	);
+};
+
+const Wrapper = styled.div`
+	width: 75%;
+	height: 75px;
+`;
+
+const ActionBtn = styled.button`
+	width: 100%;
+	color: #fff;
+	background-color: #333;
+	font-size: 4rem;
+`;
